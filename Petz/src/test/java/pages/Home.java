2e4c80753f25508ccda5f83614b1e9971344013b
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2021-01-28 22:37:23 -0300

    Estrutura de mapeammentos | acessando e pesquisando item

---
+++ Petz/src/test/java/pages/Home.java
@@ -0,0 +1,58 @@
+package pages;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Home extends PageObject {
+	//atributos
+	//caixa de pesquisa
+	@FindBy(id="search")
+	private WebElement busca;
+	
+	
+	//contrutor
+	public Home(WebDriver driver) {
+		super(driver);
+	}
+	
+	
+	
+	
+	//metodos e funcoes
+	public void buscaProdutoComEnter(String produto) {
+		busca.clear();
+		busca.sendKeys(produto+ Keys.ENTER);//escrever na caiza de
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
