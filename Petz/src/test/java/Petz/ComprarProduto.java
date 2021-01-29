ba5c28683f4e2b6ea116ecd3aa2e628aa853ae58
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2021-01-28 21:44:50 -0300

    Adicionando arquivo com passos para execução dos testes

---
+++ Petz/src/test/java/Petz/ComprarProduto.java
@@ -0,0 +1,74 @@
+package petz;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Entao;
+import cucumber.api.java.pt.Quando;
+
+public class ComprarProduto {
+//3.1 Atributos
+	String url;
+	WebDriver driver;//objeto do selenium Web drive
+	
+	
+	//3.2 Metodos ou funções
+	
+	
+	@Before//antes do teste
+	public void iniciar() {
+		url = "https://www.petz.com.br";
+		
+		System.setProperty("webdriver.chrome.driver","drivers/88/chromedriver.exe");
+		driver =new ChromeDriver();
+		driver.manage().window().maximize();//maximizar a janela do brozer
+		driver.manage().timeouts().implicitlyWait(30000, TimeUnit.MILLISECONDS);//tempo de esera
+	}
+	@After //Depois do teste
+	public void finalizar() {
+		driver.quit();
+	}
+	
+	@Test //O teste
+	public void abrirPagina() {
+		driver.get(url);//abre a pagina do site
+		assertEquals("Pet Shop: Petz o maior pet shop do Brasil",driver.getTitle());//validar o text
+	}
+
+	
+	@Dado("^que acesso o site Petz$")
+	public void que_acesso_o_site_Petz() {
+	    
+	}
+	
+	@Quando("^busco por \"([^\"]*)\" e pressiono Enter$")
+	public void busco_por_e_pressiono_Enter(String arg1){
+	   
+	}
+	
+	@Entao("^exibe uma lista de produtos relacionados com \"([^\"]*)\"$")
+	public void exibe_uma_lista_de_produtos_relacionados_com(String arg1) throws Throwable {
+	    
+	}
+	
+	@Quando("^escolho \"([^\"]*)\"$")
+	public void escolho(String arg1){
+	    
+	}
+	
+	@Entao("^exibe para o \"([^\"]*)\" o preco de \"([^\"]*)\"$")
+	public void exibe_para_o_o_preco_de(String arg1, String arg2) {
+	    
+	}
+	
+
+}
--- Petz/src/test/resources/ComprarProduto.feature
+++ Petz/src/test/resources/ComprarProduto.feature
@@ -2,12 +2,12 @@
 
 Funcionalidade: Comprar Produto
 	#Fluxo de compra no site Petz
-	#Envolve as páginas Home,Resultados da Consulta e Detalhes Páginas
+	#Envolve as páginas Home,Resultados da Consulta e Detalhes Paginas
 	
 	Cenario: Compra a partir de uma busca
 	Dado que acesso o site Petz 
 	Quando busco por "coleira" e pressiono Enter
-	Então exibe uma lista de produtos relacionados com "coleira"
-	Quando escolho "Coleira Petz Azul pra Cães"
+	Entao exibe uma lista de produtos relacionados com "coleira"
+	Quando escolho "Coleira Petz Azul pra Caes"
 	Entao exibe para o "Tamanho P" o preco de "RS 39,99" 
 	
